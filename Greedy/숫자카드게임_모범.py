05c5b03e6a17d7c1804a4e46aa85e2df0c04ac65
Eunjin <80869040+eunjin-jun717@users.noreply.github.com>
2022-02-12 14:29:21 +0900

    Create 숫자카드게임_모범.py

---
+++ Greedy/숫자카드게임_모범.py
@@ -0,0 +1,18 @@
+#p98
+#숫자카드게임
+#이중for문 이용하는 방법
+
+n,m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+  data = list(map(int, input().split()))
+  #현재 줄에서 가장 작은 수 찾기
+  min_value = 10001
+  for a in data:
+    min_value = min(min_value, a)
+   #'가장 작은 수'들 중에서 가장 큰 수 찾기
+  result = max(result, min_value)
+ 
+print(result)
